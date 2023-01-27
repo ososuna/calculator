b84e8668c957e3d391494904133b684a6008d0e6
ososuna <oswaldo.adrian@live.com.mx>
2023-01-27 17:36:51 -0600

    buttons and styles finished

--- src/components/ButtonComponent.tsx
+++ src/components/ButtonComponent.tsx
@@ -1,16 +1,29 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { styles } from '../theme/appTheme';
 
 interface Props {
   text: string;
   color?: string;
+  wide?: boolean;
 }
 
-export const ButtonComponent = ({ text, color = '#2D2D2D' }: Props) => {
+export const ButtonComponent = ({ text, color = '#2D2D2D', wide = false }: Props) => {
   return (
-    <View style={{ ...styles.button, backgroundColor: color }}>
-      <Text style={styles.textButton}>{ text }</Text>
-    </View>
+    <TouchableOpacity>
+      <View style={{
+        ...styles.button,
+        backgroundColor: color,
+        width: ( wide ) ? 180 : 80,
+      }}>
+        <Text style={{
+          ...styles.textButton,
+          color: ( color ) === '#9B9B9B' ? 'black' : 'white',
+        }}>
+          { text }
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 };
--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -6,14 +6,44 @@ import { styles } from '../theme/appTheme';
 export const CalculatorScreen = () => {
   return (
     <View style={ styles.calculatorContainer }>
+
       <Text style={ styles.resultLittle }>1500</Text>
       <Text style={ styles.result }>1500</Text>
+
       <View style={ styles.row }>
         <ButtonComponent text="C" color="#9B9B9B" />
-        <ButtonComponent text="C" color="#9B9B9B" />
-        <ButtonComponent text="C" />
-        <ButtonComponent text="C" color="#FF9427" />
+        <ButtonComponent text="+/-" color="#9B9B9B" />
+        <ButtonComponent text="del" color="#9B9B9B" />
+        <ButtonComponent text="/" color="#FF9427" />
       </View>
+
+      <View style={ styles.row }>
+        <ButtonComponent text="7" />
+        <ButtonComponent text="8" />
+        <ButtonComponent text="9" />
+        <ButtonComponent text="X" color="#FF9427" />
+      </View>
+
+      <View style={ styles.row }>
+        <ButtonComponent text="4" />
+        <ButtonComponent text="5" />
+        <ButtonComponent text="6" />
+        <ButtonComponent text="-" color="#FF9427" />
+      </View>
+
+      <View style={ styles.row }>
+        <ButtonComponent text="1" />
+        <ButtonComponent text="2" />
+        <ButtonComponent text="3" />
+        <ButtonComponent text="+" color="#FF9427" />
+      </View>
+
+      <View style={ styles.row }>
+        <ButtonComponent text="0" wide />
+        <ButtonComponent text="." />
+        <ButtonComponent text="=" />
+      </View>
+
     </View>
   );
 };
