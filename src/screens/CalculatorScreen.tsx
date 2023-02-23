c2a0199d3922ad40d18fba0d7ef4a30842e1672e
ososuna <oswaldo.adrian@live.com.mx>
2023-02-23 17:13:37 -0600

    build number validations

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -13,7 +13,34 @@ export const CalculatorScreen = () => {
   };
 
   const buildNumber = ( textNumber: string ) => {
-    setNumber( number === '0' ? textNumber : number + textNumber );
+    if ( number.includes('.') && textNumber === '.' ) { return; } // Do not accept double point 
+    if ( number.startsWith('0') || number.startsWith('-0') ) {
+      // Decimal point
+      if ( textNumber === '.' ) {
+        setNumber( number + textNumber );
+      // Evaluate if it is another zero and there is no point
+      } else if ( textNumber === '0' && number.includes('.') ) {
+        setNumber( number + textNumber );
+      // Evaluate if it is different from zero and there is no point
+      } else if ( textNumber !== '0' && !number.includes('.') ) {
+        setNumber( textNumber );
+      // Avoid 000.0
+      } else if ( textNumber === '0' && !number.includes('.') ) {
+        setNumber( number );
+      } else {
+        setNumber( number + textNumber );
+      }
+    } else {
+      setNumber( number + textNumber );
+    }
+  };
+
+  const positiveNegative = () => {
+    if ( number.includes('-') ) {
+      setNumber( number.replace('-', '') );
+    } else {
+      setNumber( '-' + number );
+    }
   };
 
   return (
@@ -30,7 +57,7 @@ export const CalculatorScreen = () => {
 
       <View style={ styles.row }>
         <ButtonComponent text="C" color="#9B9B9B" action={ clean } />
-        <ButtonComponent text="+/-" color="#9B9B9B" action={ clean } />
+        <ButtonComponent text="+/-" color="#9B9B9B" action={ positiveNegative } />
         <ButtonComponent text="del" color="#9B9B9B" action={ clean } />
         <ButtonComponent text="/" color="#FF9427" action={ clean } />
       </View>
